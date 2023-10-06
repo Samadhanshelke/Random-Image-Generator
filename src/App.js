1acf70efe58a037a0a7487ec90a7d5e2967f7c23
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2023-10-06 09:11:09 +0530

    small fix

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import { useState } from "react";
 import SearchImages from "./api";
 import SearchBar from "./components/SearchBar";
 import ImageList from "./components/ImageList";
+import PreRender from "./components/PreRender";
 function App() {
   const [images,setImages] = useState([]);
   const handleSubmit = async(term)=>{
    const result = await SearchImages(term);
+  
       setImages(result);
 
   };
+  
   return (
   <>
     <SearchBar onSubmit ={handleSubmit}/>
-    <ImageList images={images}/>
+
+    {images.length <= 0 ? <PreRender/> :  <ImageList images={images}/>}
   </>
   );
 }
---
+++ src/components/PreRender.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PreRender.css'
+const PreRender = () => {
+  return (
+    <div className='container'>
+    <h1>
+        Search Your Favourite Image
+
+    </h1>
+    </div>
+  )
+}
+
+export default PreRender
